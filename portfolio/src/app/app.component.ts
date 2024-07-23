e56376e0494dfe421700632987596a41b10d18cc
Giuseppe <giuseppedibenedetto654@gmail.com>
2024-07-23 18:49:33 +0200

    Aggiunta meta-description

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  constructor(private meta: Meta) {}
+
+  ngOnInit() {
+    this.meta.addTag({ name: 'description', content: 'Ciao, sono Giuseppe Di Benedetto, sono un junior fullstack developer. Scopri il mio portfolio, i miei progetti e come contattarmi.' });
+  }
 }
